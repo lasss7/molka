499c3a474cde8355f2ffce3f0576262290fe35f9
Adem Saidi <ademsaidisd@gmail.com>
2026-02-08 23:03:07 +0100

    add analytics

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/valentine/",
+  base: "/molka/",
   plugins: [react()],
   server: {
     allowedHosts: ["lore-copious-juridically.ngrok-free.dev"]
